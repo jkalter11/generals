da5bca582fdf75b1358074366f21e372ade8796a
jojo <mttacder13@yahoo.com>
2013-11-26 00:11:29 +0800

    removed site counter and index router since it is not being used due to express.static

--- app/server/router.js
+++ app/server/router.js
@@ -1,17 +1,9 @@
-// don't need to be persisted, resets after every deploy
-var siteVisitCounter = 0;
-
 /**
  * Most of our game logic is done with Socket.IO
  * @param  {Object} app        The express framework application
  * @param  {Object} controller The game controller which handles our games
  */
 exports.handle = function(app, controller, config) {
-
-    app.get('/', function(request, response) {
-        siteVisitCounter++;
-        response.render('index.html');
-    });
 
     app.get('/purge-db', function(request, response) {
         response.send({
